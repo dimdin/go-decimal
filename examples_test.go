4e2fec362ca2e48a81fff09c8b58d2e0b26d8a2a
Dimitris Dinodimos <dimdin@gmail.com>
2014-06-16 18:44:48 +0300

    add more examples

--- examples_test.go
+++ examples_test.go
@@ -6,21 +6,54 @@ package decimal
 
 import "fmt"
 
+func ExampleDec_Cmp() {
+	var x, y Dec
+	x.SetString("1.1")
+	y.SetString("2.0")
+	if x.Cmp(&y) < 0 {
+		fmt.Printf("%s < %s", x, y)
+	}
+	// Output:
+	// 1.1 < 2.0
+}
+
+func ExampleNullDec_Cmp() {
+	var x, y NullDec
+	x.SetString("1.1")
+	y.SetString("2.0")
+	if x.Cmp(&y) < 0 {
+		fmt.Printf("%s < %s", x, y)
+	}
+	// Output:
+	// 1.1 < 2.0
+}
+
+func ExampleDec_Neg() {
+	var d Dec
+	d.SetString("12.34")
+	d.Neg(&d)
+	fmt.Println(d)
+	// Output:
+	// -12.34
+
+}
+
+func ExampleNullDec_Neg() {
+	var d NullDec
+	d.SetString("12.34")
+	d.Neg(&d)
+	fmt.Println(d)
+	// Output:
+	// -12.34
+
+}
+
 func ExampleDec_SetString() {
-	d := new(Dec)
+	var d Dec
 	d.SetString("-12.34")
 	fmt.Println(d)
 	// Output:
 	// -12.34
-}
-
-func ExampleDec_SetBytes() {
-	d := new(Dec)
-	bytes := []byte("+12.34")
-	d.SetBytes(bytes)
-	fmt.Println(d)
-	// Output:
-	// 12.34
 }
 
 func ExampleNullDec_SetString() {
@@ -31,11 +64,30 @@ func ExampleNullDec_SetString() {
 	// 12.34
 }
 
+func ExampleDec_SetBytes() {
+	var d Dec
+	bytes := []byte("+12.34")
+	d.SetBytes(bytes)
+	fmt.Println(d)
+	// Output:
+	// 12.34
+}
+
 func ExampleNullDec_SetBytes() {
 	var d NullDec
 	bytes := []byte("-12.34")
 	d.SetBytes(bytes)
 	fmt.Println(d)
+	// Output:
+	// -12.34
+}
+
+func ExampleNullDec_Null() {
+	var d NullDec
+	d.SetString("-12.34")
+	if !d.Null() {
+		fmt.Println(d)
+	}
 	d.SetString("")
 	if d.Null() {
 		fmt.Println("null")
@@ -45,20 +97,20 @@ func ExampleNullDec_SetBytes() {
 	// null
 }
 
-func ExampleNullDec_Null() {
-	var d NullDec
-	d.SetString("-12.34")
-	if !d.Null() {
-		fmt.Println(d)
-	}
-	// Output:
-	// -12.34
-}
-
 func ExampleNullDec_SetInt128() {
 	var i Int128
 	i.SetInt64(100000)
 	var d NullDec
+	d.SetInt128(&i)
+	fmt.Println(d)
+	// Output:
+	// 100000
+}
+
+func ExampleDec_SetInt128() {
+	var i Int128
+	i.SetInt64(100000)
+	var d Dec
 	d.SetInt128(&i)
 	fmt.Println(d)
 	// Output:
@@ -75,6 +127,24 @@ func ExampleNullDec_SetDec() {
 	// 1.2
 }
 
+func ExampleNullDec_Dec() {
+	var d NullDec
+	d.SetString("1.2")
+	fmt.Println(d.Dec())
+	// Output:
+	// 1.2
+}
+
+func ExampleDec_Add() {
+	var x, y Dec
+	x.SetString("0.1")
+	y.SetInt64(1)
+	x.Add(&x, &y)
+	fmt.Println(x)
+	// Output:
+	// 1.1
+}
+
 func ExampleNullDec_Add() {
 	var x, y NullDec
 	x.SetString("0.1")
@@ -85,6 +155,16 @@ func ExampleNullDec_Add() {
 	// 1.1
 }
 
+func ExampleDec_Mul() {
+	var x, y Dec
+	x.SetString("1.1")
+	y.SetInt64(2)
+	x.Mul(&x, &y)
+	fmt.Println(x)
+	// Output:
+	// 2.2
+}
+
 func ExampleNullDec_Mul() {
 	var x, y NullDec
 	x.SetString("1.1")
@@ -93,6 +173,16 @@ func ExampleNullDec_Mul() {
 	fmt.Println(x)
 	// Output:
 	// 2.2
+}
+
+func ExampleDec_Div() {
+	var x, y Dec
+	x.SetInt64(100)
+	y.SetInt64(3)
+	x.Div(&x, &y, 2)
+	fmt.Println(x)
+	// Output:
+	// 33.33
 }
 
 func ExampleNullDec_Div() {
